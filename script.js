ebe3cde9b1f1d9c87268bdbb09002da9d281a9a7
simone-art <simoneperiodista3@gmail.com>
2019-12-01 20:33:45 -0300

    realizada la segunda y tercera página web de html, css y javascript

---
+++ script.js
@@ -0,0 +1,49 @@
+// formulario-regístrese//
+
+adicioneNome.addEventListener("click", function(evento){
+    evento.preventDefault();
+    if (adicioneNome.value.trim() == ""){
+        adicioneNome.setAttribute("placeholder","Por favor, digite o seu nome");
+    } else {
+        adicioneNome.setAttribute("placeholder", " ")
+    }
+})
+
+adicioneSobrenome.addEventListener("click", function(evento){
+    evento.preventDefault();
+    if (adicioneSobrenome.value.trim() == ""){
+        adicioneSobrenome.setAttribute("placeholder","Por favor, digite o seu sobrenome");
+    }
+})
+
+
+
+adicioneRg.addEventListener("click", function(evento){
+    evento.preventDefault();
+    if (adicioneRg.value.trim() == ""){
+        adicioneRg.setAttribute("placeholder","Por favor, digite o seu RG");
+    }
+})
+
+adicioneSenha.addEventListener("click", function(evento){
+    evento.preventDefault();
+    if (adicioneSenha.value.trim() == ""){
+        adicioneSenha.setAttribute("placeholder","Por favor, insira a sua nova senha");
+    }    
+})
+
+confirmeSenha.addEventListener("click", function(evento){
+    evento.preventDefault();
+    if (confirmeSenha.value.trim() == ""){
+        confirmeSenha.setAttribute("placeholder","Por favor, confirme a sua nova senha");
+    }    
+})
+
+//BUTTON ENVIAR//
+
+let btnEnviar = document.getElementById("form")
+
+btnEnviar.addEventListener("click", function (e){
+    evento.preventDefault();
+})
+console.log("clicou")
