db7fcfc302814f438b978ab0610f8dd478c4fb29
simone-art <simoneperiodista3@gmail.com>
2019-12-05 22:29:03 -0300

    creada responsividad en index.html, página 1, en css

--- script3.js
+++ script3.js
@@ -1,13 +1,32 @@
+//BUTTONCADERNO//
+
+// btnCadernos = document.getElementsByClassName("btn-cadernos")
+
+// btnCadernos.addEventListener("click", function (evento){
+    
+// })
+
+
 //TEXTAREA//
 
 // let textoCaderno = document.getElementById("cadernoReprograma")
 
 //BUTTONSALVAR//
 
-let btnSalvar = document.getElementById("salvar")
+const btnSalvar = document.getElementById("salvar")
 
 btnSalvar.addEventListener("click", function (evento){
     evento.preventDefault();
 })
 
 console.log("salvar")
+
+//BUTTONNOVO/
+
+// let btnNovo = document.getElementById("novo")
+
+// btnSalvar.addEventListener("click", function (evento){
+//     evento.preventDefault();
+// })
+
+// console.log("novo")
