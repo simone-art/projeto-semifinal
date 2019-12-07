a77d2ad8b5f49077fd4ec5a17b86f5ff2af17308
simone-art <simoneperiodista3@gmail.com>
2019-12-07 15:34:19 -0300

    funciona el código del botón agregar de la página 3

--- script3.js
+++ script3.js
@@ -1,24 +1,45 @@
-//BUTTONCADERNO//
+//BUTTONCADERNO FUNCIONALIDAD: BUTTON AGREGAR CREA UNA LI EN EL MISMO
+//ESPACIO DONDE ESTÁ EL BUTTONCADERNO//
 
-const buttonCadernos = document.querySelector(".btn-cadernos");
+// const buttonCadernos = document.getElementById("button-cadernos");
+
+// const inputValue = document.getElementById("input-cadernos-texto");
+// const buttonAgregar = document.getElementById("button-agregar");
+// const adicioneTexto = document.getElementById("input-caderno-texto");
+
+
+const buttonAgregar = document.getElementById("button-agregar");
+const tituloInput= document.getElementById("tituloInput") 
 const error = document.querySelector(".error");
 
-btnCriarCadernos = document.getElementById("button-cadernos")
-// const texto = document.getElementById("adicione-caderno")
-
-btnCriarCadernos.addEventListener("click", function (evento){
-    let inputCadernoTexto = texto.value;
+buttonAgregar.addEventListener("click", function(evento){
+    console.log("agregar")
+    let tabela = document.getElementById("Tabela");
+    let titulo= tituloInput.value;
     evento.preventDefault();
-    if (inputCadernoTexto === "") {
-        erro.textContent= "error"
-    } else{
-        let buttonCaderno= document.createElement("button");
-        buttonCaderno.textContent = inputCadernoTexto;
-        buttonCaderno.classList.add("input-caderno");
-        inputCadernoTexto.appendChild(buttonCaderno);
-        inputCadernoTexto = "";
+    if (titulo === "") {
+        error.textContent = "Digite o título da sua matéria!"
+    } else {
+        let liTabela = document.createElement("li");
+        liTabela.textContent = titulo;
+        tabela.appendChild(liTabela);
+        tabela = "";
         error.textContent = "";
     }
+});
+
+
+
+
+// btnCriarCadernos = document.getElementById("button-cadernos")
+
+// btnCriarCadernos.addEventListener("click", function (evento){
+//         let buttonCaderno= document.createElement("button");
+//         buttonCaderno.classList.add("btn-cadernos");
+//         inputCadernoTexto.appendChild(buttonCaderno);
+//         inputCadernoTexto = "";
+//         error.textContent = "";
+//     }
 
     // console.log("criar")
     
